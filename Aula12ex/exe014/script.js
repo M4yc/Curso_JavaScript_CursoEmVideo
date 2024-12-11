574fe7b640358f29766808458f3247bdf144dea9
Maycon Vinicius <mayconvbatista84@gmail.com>
2024-12-11 09:51:15 -0300

    🆙 Update exe014

--- Aula12ex/exe014/script.js
+++ Aula12ex/exe014/script.js
@@ -1,20 +1,23 @@
-var agora = new Date()
-var hora = agora.getHours()
-var bodyjs = document.querySelector('body')
-var divtxt = document.getElementById('divtitle')
-var imgjs = document.getElementById('img')
+function carregar(){
+    var agora = new Date()
+    var hora = agora.getHours()
+    var bodyjs = document.querySelector('body')
+    var msgtxt = document.getElementById('msg')
+    var img = document.getElementById('imagem')
 
-hora = 14
+    msgtxt.innerText = `Agora são ${hora} horas.`
 
-divtxt.innerText = `Agora são ${hora} horas.`
-
-if (hora < 12){
-    bodyjs.style.backgroundImage = 'url(./assets/dia.jpg)'
-    imgjs.src = './assets/dia.jpg'
-}else if(hora < 18){
-    bodyjs.style.backgroundImage = 'url(./assets/tarde.jpg)'
-    imgjs.src = './assets/tarde.jpg'
-}else if(hora >= 18){
-    bodyjs.style.backgroundImage = 'url(./assets/noite.jpg)'
-    imgjs.src = './assets/noite.jpg'
+    if (hora >= 5 && hora < 12){
+        bodyjs.style.backgroundImage = 'url(./assets/dia.jpg)'
+        img.src = './assets/dia.jpg'
+    }else if(hora >= 12 && hora < 18){
+        bodyjs.style.backgroundImage = 'url(./assets/tarde.jpg)'
+        img.src = './assets/tarde.jpg'
+    }else if(hora >= 18 && hora < 24){
+        bodyjs.style.backgroundImage = 'url(./assets/noite.jpg)'
+        img.src = './assets/noite.jpg'
+    }else{
+        bodyjs.style.backgroundImage = 'url(./assets/madrugada.jpg)'
+        img.src = './assets/madrugada.jpg'
+    }
 }
